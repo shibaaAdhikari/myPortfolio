100f45105b71e76dcdd699f9043d70518b3e877f
Shibaa <shibaaadhikari0@gmail.com>
2023-04-12 15:23:51 +0545

    sliding the image in a row

--- src/Components/ExceptionalWorks/ExceptionalWorks.jsx
+++ src/Components/ExceptionalWorks/ExceptionalWorks.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import "./ExceptionalWorks.css";
-import WorkI from "../../Assests/workI.png";
+import WorkI from "../../Assests/expI.jpg";
 import WorkII from "../../Assests/workII.png";
-import WorkIII from "../../Assests/workIII.png";
-import workIV from "../../Assests/WorkIV.png";
-import { Swiper, SwiperSlide, useSwiperSlide } from 'swiper/react';
+import WorkIII from "../../Assests/expII.jpg";
+import workIV from "../../Assests/expIII.jpg";
+import { Swiper, SwiperSlide} from 'swiper/react';
 import "swiper/css";
-import "swiper/css/bundle"
-import { Navigation, Pagination, Scrollbar, A11y} from 'swiper';
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+import { EffectCoverflow, Pagination } from "swiper";
 // import { useSwiperSlide } from "swiper/react";
+
 const ExceptionalWorks = () => {
-  const swiperSlide = useSwiperSlide();
+ 
 
   return (
     <>
@@ -21,18 +24,22 @@ const ExceptionalWorks = () => {
           </p>
 
           <Swiper
-            // install Swiper modules
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
-            spaceBetween={50}
-            slidesPerView={3}
-            navigation
-            pagination={{ clickable: true }}
-            scrollbar={{ draggable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log('slide change')}
-          
-
-          >
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"2"}
+        // spaceBetween={60}
+        coverflowEffect={{
+          rotate: 60,
+          stretch: 0,
+          depth: 50,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
             <SwiperSlide>
               <img src={WorkI} alt="images" 
               
@@ -40,7 +47,7 @@ const ExceptionalWorks = () => {
             </SwiperSlide>
             <SwiperSlide >
               <img src={WorkII} alt="images" 
-              className={swiperSlide?.isActive ? 'active' : 'not active'}
+             
               />
             </SwiperSlide>
             <SwiperSlide>
@@ -49,7 +56,7 @@ const ExceptionalWorks = () => {
             <SwiperSlide>
               <img src={workIV} alt="images" />
             </SwiperSlide>
-            ........
+        
           </Swiper>
         </div>
       </div>
