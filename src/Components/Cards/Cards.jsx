5149c8e3de9bcf4057f152d548aeaf3394e1dbba
Shibaa <shibaaadhikari0@gmail.com>
2023-04-20 14:53:44 +0545

    added socialMedia icon

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -4,13 +4,13 @@ import "./Cards.css";
 const Cards = ({ items, className }) => {
   return (
     <>
-      <div>
+      <div className="cardDiv">
         <div className={`baseCard ${className}`}>
           <div>
             <img src={items.image} alt={""} className="image" />
           </div>
           <div className="cards_title">{items.title}</div>
-          <div className="cards_descriptions">{items.descriptions}</div>
+          {/* <div className="cards_descriptions">{items.descriptions}</div> */}
         </div>
       </div>
     </>
---
+++ src/Pages/Practice/Practice.jsx
@@ -0,0 +1,19 @@
+// import React from "react";
+// import "./Practice.css";
+
+// const Practice = ({ practiceRef }) => {
+//   return (
+//     <>
+//       <main ref={practiceRef} className="maindiv">
+//         <div className="div1">div1</div>
+//         <div className="div2">div2</div>
+//         <div className="div3">div3</div>
+//         <div className="div4">div4</div>
+//         <div className="div5">div5</div>
+//         <div className="div6">div6</div>
+//       </main>
+//     </>
+//   );
+// };
+
+// export default Practice;
